6b61e3a36647b661f959e78a37d61c611b4a7639
nina <ninaw2306@gmail.com>
2025-03-19 17:20:54 +0800

    Style: 調整圖片路徑

--- src/views/user/CartLayout.vue
+++ src/views/user/CartLayout.vue
@@ -34,7 +34,7 @@ export default {
 </script>
 
 <template>
-  <img src="https://storage.googleapis.com/vue-course-api.appspot.com/juiceoasis/1712727758586.jpg?GoogleAccessId=firebase-adminsdk-zzty7%40vue-course-api.iam.gserviceaccount.com&Expires=1742169600&Signature=VijqnQgQPHGlMHnrLQK3fr49TM1uNZIolt03Z1MFugnj4lax8sLDzabDxyS3GbVWsOzuqxl5R6YB4u1sfEAMY%2BYfzDShj0yvwWL7%2FG0pDCrwTcWFBX%2BxcmIKfsNB1pPmIRp1QUseI5mBqKdOaaQJHC0VhoAu11fGTz%2FdbocytEwRANrCzy8Wh0FjdMq4Dvgz6Y3a5YWDEq8wngqVNJOrRTuIy1kSJVIWlLfg84UD4fEzgGyrfddoHmXnlByg3joUsMTco%2BSBQahIMUUEocPxHTp36OFML49PqisDUI6%2F80CnlylBxJKiWlS4hWq6pqS%2Fl5j3HqNVKT%2FUvpYd3H7rQQ%3D%3D" alt="購物車結帳背景圖" class="bgImage"
+  <img src="https://res.cloudinary.com/derxrcfij/image/upload/v1742289766/%E8%B3%BC%E7%89%A9%E8%BB%8A_r22hlv.jpg" alt="購物車結帳背景圖" class="bgImage"
     style="width: 100%; object-fit: cover; object-position: center center;">
   <div class="container py-5">
     <template v-if="cartInfo.list?.length">
